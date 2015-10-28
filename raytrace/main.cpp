d12b4642022d2dd1f3ad60493f391045386868a7
Daniel Fuller <lasaldan@gmail.com.com>
2015-10-27 21:37:21 -0600

    Initial Commit

---
+++ raytrace/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  raytrace
+//
+//  Created by Daniel Fuller on 10/27/15.
+//  Copyright (c) 2015 Daniel Fuller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
